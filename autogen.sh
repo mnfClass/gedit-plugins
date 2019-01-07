021f6d68ed967134c36e97d69913f7608735888e
Jeremy Bicha <jbicha@ubuntu.com>
2019-01-06 23:02:15 -0500

    build: Stop using intltool
    Recent gettext version can extract and merge back strings from and to
    various file formats, no need for intltool anymore.
    
    https://wiki.gnome.org/Initiatives/GnomeGoals/GettextMigration

--- autogen.sh
+++ autogen.sh
@@ -7,20 +7,13 @@ test -n "$srcdir" || srcdir=.
 olddir=`pwd`
 cd "$srcdir"
 
-INTLTOOLIZE=`which intltoolize`
-if test -z $INTLTOOLIZE; then
-        echo "*** No intltoolize found, please install the intltool package ***"
-        exit 1
-fi
-
 AUTORECONF=`which autoreconf`
 if test -z $AUTORECONF; then
         echo "*** No autoreconf found, please install it ***"
         exit 1
 fi
 
-autopoint --force
-AUTOPOINT='intltoolize --automake --copy' autoreconf --force --install --verbose
+autoreconf --force --install --verbose
 
 cd "$olddir"
 test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
